672f835cacdfa5b3178fb3a1bf9addf78e44748c
Adam Priest <sepone229@gmail.com>
2020-03-13 23:50:41 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,8 +10,12 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday
+def age_appropriate_birthday(birthday_kids)
   i = 0
   if i > 12
-    
+    puts "You are too old for this."
+  else
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
 
